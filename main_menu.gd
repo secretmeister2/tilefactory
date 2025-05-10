700b1de058d6fca47d3b6dbc92ea2aa00ef81cd0
secretmeister2 <26khoffman@isstavanger.no>
2025-05-10 20:46:29 +0200

    change tilesetids

--- main_menu.gd
+++ main_menu.gd
@@ -2,13 +2,6 @@ extends Control
 var target_pos
 func _process(float)->void:
 	if not target_pos: target_pos=$Camera2D.position
-	if Input.is_action_just_pressed("ui_select"):
-		$NoiseGenerator.generate()
 	var thing = Input.get_vector("ui_right","ui_left","ui_down","ui_up")
 	target_pos -= 3*thing
 	$Camera2D.position+=0.05 * (target_pos- $Camera2D.position)
-
-
-func panelclick(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		print($TileMapLayer.get_cell_alternative_tile($TileMapLayer.local_to_map(event.global_position())))
--- tilescenes/tile.gd
+++ tilescenes/tile.gd
@@ -1,5 +1,4 @@
 extends Node2D
-var grid:GaeaGrid
 var coords:Vector2i
 func mousereact(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
